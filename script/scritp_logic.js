121ac9af72d8e07e4e03fe136b2c9c17f025b5de
isaque <isaquesevero8305@gmail.com>
2025-11-11 18:28:11 -0300

    Update scritp_logic.js

--- script/scritp_logic.js
+++ script/scritp_logic.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     function e() { $("#fb").attr("href", "https://www.facebook.com/profile.php?id=100074620631348"), $("#ins").attr("href", "https://www.instagram.com/jorge_devs/"), $("#lins").attr("href", "https://www.linkedin.com/in/jorge-devs") } function t() { $("#close").on("click", () => { $("#wath").attr("src", ""), $("#model_play").css("display", "none") }) } function o() {
-        let e = document.getElementById("search").value, t = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(e)}&type=video&maxResults=10&order=viewCount&videoEmbeddable=true&regionCode=BR&key=AIzaSyACT9ylFwpm8PoGjORzTClxGEt0yfzHxjY`; fetch(t).then(e => e.json()).then(e => {
+        let e = document.getElementById("search").value, t = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(e)}&type=video&maxResults=10&order=viewCount&videoEmbeddable=true&regionCode=BR&key=AIzaSyDxaokMs6Byzpy1N4FVis0XZCcBqj5XldY`; fetch(t).then(e => e.json()).then(e => {
             let t = document.getElementById("box_video"); t.innerHTML = "", e.items.forEach(e => {
                 let o = e.id.videoId, a = e.snippet.thumbnails.high.url, i = e.snippet.title, s = document.createElement("div"); s.onclick = () => n(o), s.innerHTML = `
     <img src="${a}" alt="Imagem">
@@ -14,3 +14,4 @@ Resultado
 Pesquise alguma coisa
 </h2>`), $("#search").on("keydown", function (e) { ("Enter" === e.key || 13 === e.keyCode) && (o(), $("#search").val("")) }), e(), o(), t()
 });
+
